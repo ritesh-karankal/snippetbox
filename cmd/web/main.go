402c1ceb49330ffe0855c086537264b883e480ba
Ritesh Karankal <ritesh.karankal200@gmail.com>
2025-01-04 16:40:51 +0530

    Inject dependencies

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,20 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	
+	app := &application{
+		logger: logger,
+	}
 
 	mux := http.NewServeMux()
 
@@ -20,16 +28,15 @@ func main() {
 
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-        mux.HandleFunc("GET /{$}", home)
-        mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-        mux.HandleFunc("GET /snippet/create", snippetCreate)
+        mux.HandleFunc("GET /{$}", app.home)
+        mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+        mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 
-        mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+        mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	logger.Info("starting server", "addr", *addr)
 
         err := http.ListenAndServe(*addr , mux)
-
         logger.Error(err.Error())
 	os.Exit(1)
 }
