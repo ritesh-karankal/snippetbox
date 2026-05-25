9808ac34bef605d4a4538f06b31e1eb3f84105bd
Ritesh Karankal <ritesh.karankal0209@gmail.com>
2026-05-25 23:09:03 +0530

    routing requests

--- main.go
+++ main.go
@@ -9,13 +9,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
 }
 
+func viewSnippet(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Display a form for creating a new snippet..."))
+}
 
 
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", viewSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 	log.Println("Starting server at :4000")
 
 	err := http.ListenAndServe(":4000", mux)
