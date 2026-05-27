78b5c8dd97541bea8259d0711214f219db85e753
Ritesh Karankal <ritesh.karankal0209@gmail.com>
2026-05-27 15:55:41 +0530

    structured logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"flag"
-    "log"
-    "net/http"
+        "net/http"
+    "log/slog"
+    "os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
 	flag.Parse()
+	
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -20,9 +24,10 @@ func main() {
     mux.HandleFunc("GET /snippet/create", snippetCreate)
     mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-    log.Printf("starting server on %s", *addr)
+    logger.Info("starting server", "addr", *addr)
     
     err := http.ListenAndServe(*addr, mux)
-    log.Fatal(err)
+    logger.Error(err.Error())
+    os.Exit(1)
 }
 
